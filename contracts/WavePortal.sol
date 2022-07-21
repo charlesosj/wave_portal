f9421483546e56c933a694ac211cf3fedccb803f
Charles <charlesosj@outlook.com>
2022-07-21 13:14:34 +0100

    Initial commit

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Yo yo,I am a contract and i am smart")
+    }
+}
